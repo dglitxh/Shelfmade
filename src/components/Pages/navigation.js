f98e3279eb072530a7b116e148e37399b323c7cc
YD <ydglitch@gmail.com>
2022-01-11 19:56:26 +0000

    tweaks: navbar

--- src/components/Pages/navigation.js
+++ src/components/Pages/navigation.js
@@ -9,6 +9,7 @@ import { logout} from '../../Redux/userSlice';
 export const Navbar = (props) =>{
   const dispatch = useDispatch()
   const [isNav, setIsNav] = useState(true)
+  const [isUserMenu, setIsUserMenu] = useState(false)
   const {totalItems, user} = props
 
   const toggleNav = () => {
@@ -16,6 +17,10 @@ export const Navbar = (props) =>{
     else setIsNav(false)
   }
 
+  const toggleUserMenu = () => {
+    if (!isUserMenu) setIsUserMenu(true)
+    else setIsUserMenu(false)
+  }
   const signout = () => {
     const auth = getAuth();
       signOut(auth).then(() => {
@@ -52,20 +57,22 @@ export const Navbar = (props) =>{
           <Link to="/shop"className="my-1 text-gray-700 dark:text-gray-500 hover:text-red-500 dark:hover:text-indigo-400 md:mx-4 md:my-0" >Shop</Link>
           {user?
               <div>
-                  <button type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="menu-button" aria-expanded="true" aria-haspopup="true">
+                  <button onClick={toggleUserMenu}   class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="menu-button" aria-expanded="true" aria-haspopup="true">
                     {user.displayName}
                   <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                   </svg>
                   </button>
-                  {
-                  <div class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                  {isUserMenu?
+                  <div class="origin-top-right absolute  right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                     <div class="py-1" role="none">
-                          <button type="submit" class="text-gray-700 block w-full text-left px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-3">
+                          <button onClick={() => {signOut()}} class="text-gray-700 block w-full text-left px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">
                             Sign out
                           </button>
                     </div>
                   </div>
+                  :
+                  <></>
                 }
             </div>
             
