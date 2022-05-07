7adf358cec00d016485aa7e066fc40b5f0cb3a01
YD <ydglitch@gmail.com>
2022-05-07 12:52:45 +0000

    refac: forgotpwd

--- src/components/Pages/ForgotPwd.js
+++ src/components/Pages/ForgotPwd.js
@@ -1,5 +1,5 @@
 
-import useState from 'react'
+import {useState} from 'react'
 
 const ForgotPwd = () => {
     const [mail, setMail] = useState('')
@@ -8,8 +8,8 @@ const ForgotPwd = () => {
 
             <div>
                <div className="container">
-                <div className="lg:w-1/2 xl:max-w-screen-sm">
-                    <div className="mt-10 px-12 sm:px-24 md:px-48 lg:px-48 lg:mt-16 2xl:px-24 ">
+                <div className=" ">
+                    <div className="mt-10 px-12 sm:px-24 md:px-48 lg:px-48 lg:mt-16 xl:px-24 ">
                         <h2 className="text-center text-4xl text-red-600 font-display font-semibold lg:text-left xl:text-5xl
                         xl:text-bold">Forgot Password</h2>
 
@@ -22,7 +22,8 @@ const ForgotPwd = () => {
                                         placeholder="example@email.com"
                                         name='email'
                                         required={true}
-                                        onChange={mail => {setMail(e.target.value)}}/>
+                                        value={mail}
+                                        onChange={mail=>setMail(mail)}/>
                                 </div>
 
                                 <div className="mt-10">
