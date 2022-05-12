9cfef9084671a98df87a3361dd801817b86653a5
YD <ydglitch@gmail.com>
2022-05-12 13:17:25 +0000

    payment helper

--- src/components/Pages/checkout.js
+++ src/components/Pages/checkout.js
@@ -1,7 +1,19 @@
 const checkout = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+  let handler = PaystackPop.setup({
+    key: 'YOUR_PUBLIC_KEY',
+    currency: 'GHS',
+    ref: 'YOUR_REFERENCE',
+    callback: function(response) {
+      //this happens after the payment is completed successfully
+      var reference = response.reference;
+      alert('Payment complete! Reference: ' + reference);
+      // Make an AJAX call to your server with the reference to verify the transaction
+
+    },
+    onClose: function() {
+      alert('Transaction was not completed, window closed.');
+    },
+  });
+  handler.openIframe();
+  }
 }
