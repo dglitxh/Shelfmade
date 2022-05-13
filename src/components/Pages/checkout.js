5b04d757d1acbedde3af9d4690722458d3b45d89
YD <ydglitch@gmail.com>
2022-05-13 13:30:03 +0000

    upg: payment handler

--- src/components/Pages/checkout.js
+++ src/components/Pages/checkout.js
@@ -1,14 +1,15 @@
+import {v4 as uuid} from 'uuid'
 export const checkout = (email, price) => {
   if (!(/^\w@\w.com$/).test(email)){
     alert("wrong email format")
-    throw("Wrong email format")
+    throw("Email format not supported")
   }
   let handler = PaystackPop.setup({
-    key: 'YOUR_PUBLIC_KEY',
+    key: "pk_test_a1d509399e526b08ee746fe6c1160670b86f44d4",
     email: email,
     amount: price*100,
     currency: 'GHS',
-    ref: 'YOUR_REFERENCE',
+    ref: uuid(),
     callback: function(response) {
       //this happens after the payment is completed successfully
       var reference = response.reference;
@@ -17,7 +18,7 @@ export const checkout = (email, price) => {
       fetch("https://shelfmade.netlify.app verify_transaction?reference="+response.reference)
         .then(res=>{
           if (!response.ok) {
-            throw ("Error ")
+            throw ("Error verification unsuccesful")
           }
           res.json()
         })
