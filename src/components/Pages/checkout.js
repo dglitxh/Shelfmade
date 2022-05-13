c03b0b8381a3e137cd82253f0158d9ebd1491930
YD <ydglitch@gmail.com>
2022-05-13 13:21:17 +0000

    payment handler

--- src/components/Pages/checkout.js
+++ src/components/Pages/checkout.js
@@ -1,6 +1,12 @@
-const checkout = () => {
+export const checkout = (email, price) => {
+  if (!(/^\w@\w.com$/).test(email)){
+    alert("wrong email format")
+    throw("Wrong email format")
+  }
   let handler = PaystackPop.setup({
     key: 'YOUR_PUBLIC_KEY',
+    email: email,
+    amount: price*100,
     currency: 'GHS',
     ref: 'YOUR_REFERENCE',
     callback: function(response) {
@@ -8,12 +14,19 @@ const checkout = () => {
       var reference = response.reference;
       alert('Payment complete! Reference: ' + reference);
       // Make an AJAX call to your server with the reference to verify the transaction
-
+      fetch("https://shelfmade.netlify.app verify_transaction?reference="+response.reference)
+        .then(res=>{
+          if (!response.ok) {
+            throw ("Error ")
+          }
+          res.json()
+        })
+        .then(data=>console.log(data))
     },
     onClose: function() {
       alert('Transaction was not completed, window closed.');
     },
   });
   handler.openIframe();
-  }
+  
 }
